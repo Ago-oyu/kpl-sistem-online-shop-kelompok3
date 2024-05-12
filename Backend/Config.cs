589920c48dbc38a810ab5d51fa0f93d4d70538aa
haidar <haidarah134@gmail.com>
2024-05-12 19:37:10 +0700

    config

---
+++ Backend/Config.cs
@@ -0,0 +1,31 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Backend
+{
+    public class Config
+    {
+        public string dataSource;
+        public static Config GetConfig (string path="./backend_config.json")
+        {
+            if (!File.Exists(path))
+            {
+                Config config = new();
+                config.SetDefault();
+                File.WriteAllText(path, JsonSerializer.Serialize(config));
+            }
+            string jsText = File.ReadAllText(path);
+
+            var options = new JsonSerializerOptions
+            {
+                IncludeFields = true
+            };
+            return JsonSerializer.Deserialize<Config>(jsText, options);
+        }
+        public void SetDefault()
+        {
+            dataSource = "./database.db";
+        }
+    }
+}
