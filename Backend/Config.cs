d34c4761247e4586efaf2976e1651847d3f9bcb0
haidar <haidarah134@gmail.com>
2024-05-13 10:22:33 +0700

    add  endpoint and port config

--- Backend/Config.cs
+++ Backend/Config.cs
@@ -7,27 +7,33 @@ namespace Backend
     public class Config
     {
         public string dataSource;
+        public string host;
+        public string port;
         public static Config GetConfig (string path="./backend_config.json")
         {
-            if (!File.Exists(path))
-            {
-                Config config = new();
-                config.SetDefault();
-                File.WriteAllText(path, JsonSerializer.Serialize(config));
-
-                return config;
-            }
-            string jsText = File.ReadAllText(path);
-
             var options = new JsonSerializerOptions
             {
                 IncludeFields = true
             };
+
+            if (!File.Exists(path))
+            {
+                Config config = new();
+                config.SetDefault();
+                File.WriteAllText(path, JsonSerializer.Serialize(config, options));
+                
+                return config;
+            }
+            string jsText = File.ReadAllText(path);
+
+            
             return JsonSerializer.Deserialize<Config>(jsText, options);
         }
         public void SetDefault()
         {
             dataSource = "./database.db";
+            host = "localhost";
+            port = "5058";
         }
     }
 }
