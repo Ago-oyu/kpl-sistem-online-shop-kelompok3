698a32cb113b08053d437340a9c276c623265fbe
haidar <haidarah134@gmail.com>
2024-06-05 20:52:28 +0700

    get page

--- DataTypes/Produk.cs
+++ DataTypes/Produk.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Text.Json;
+
 namespace DataTypes
 {
     public class Produk : Syncronizeable<Produk>
@@ -9,6 +11,35 @@ namespace DataTypes
         public Produk()
         {
 
+        }
+
+        public static async Task<List<Produk>> GetPage()
+        {
+            using var client = new HttpClient();
+
+            string requestUrl = baseUrl + "/api/getProductPage";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(requestUrl);
+
+                // Check if the request was successful (status code 200)
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    return JsonSerializer.Deserialize<List<Produk>>(responseBody);
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to call the API. Status code: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            return null;
         }
     }
 }
