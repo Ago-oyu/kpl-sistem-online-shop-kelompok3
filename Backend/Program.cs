b6c1d13182e345aee026c71e9a33841a63479169
haidar <haidarah134@gmail.com>
2024-06-09 12:00:23 +0700

    logging

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,8 +1,17 @@
 using System.Text.Json.Serialization;
 using Backend;
+using Serilog;
 
 Config config = Config.GetConfig();
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((_, serviceProvider, loggerConfiguration) =>
+{
+    
+    loggerConfiguration
+        .Enrich.FromLogContext()
+        .WriteTo.File($"Logs/Backend.log", outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception} {RequestInfo}"); // {NewLine}input:{NewLine}{RequestBody}{NewLine}output:{NewLine}{ResponseBody}
+});
 
 // Add services to the container.
 
@@ -22,6 +31,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<RequestResponseLoggingMiddleware>();
+app.UseSerilogRequestLogging(opts => opts.EnrichDiagnosticContext = LogHelper.EnrichFromRequest);
 
 app.UseHttpsRedirection();
 
