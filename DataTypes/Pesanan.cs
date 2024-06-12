2a5c14a5622c1274a25da627adc0bb39e6fcf043
haidar <haidarah134@gmail.com>
2024-06-12 13:37:47 +0700

    fix pesanan

--- DataTypes/Pesanan.cs
+++ DataTypes/Pesanan.cs
@@ -5,12 +5,12 @@ using System.Text.Json;
 namespace DataTypes
 {
     public enum StatusPesanan {packing, perjalanan, sampai}
-    public class Pesanan: Syncronizeable<Produk>
+    public class Pesanan: Syncronizeable<Pesanan>
     {
         public StatusPesanan Status {get; set;}
-        public Produk Produk {get; set;}
-        public Penjual Penjual {get; set;}
-        public Pembeli Pembeli {get; set;}
+        public string IdProduk {get; set;}
+        public string IdPenjual {get; set;}
+        public string IdPembeli {get; set;}
 
         public Pesanan(string Id=null) : base(Id)
         {
