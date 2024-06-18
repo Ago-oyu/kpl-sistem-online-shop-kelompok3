42538bbd512d0dc325173bbf1a87045d36de6025
haidar <haidarah134@gmail.com>
2024-06-18 15:22:22 +0700

    fix casting error

--- DataTypes/Pesanan.cs
+++ DataTypes/Pesanan.cs
@@ -2,6 +2,7 @@
 
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace DataTypes
 {
@@ -11,12 +12,15 @@ namespace DataTypes
         public StatusPesanan Status {get; set;}
         public string IdProduk {get; set;}
         [NotMapped]
+        [JsonIgnore]
         public Produk Produk {get; set;}
         public string IdPenjual {get; set;}
         [NotMapped]
+        [JsonIgnore]
         public Penjual Penjual {get; set;}
         public string IdPembeli {get; set;}
         [NotMapped]
+        [JsonIgnore]
         public Pembeli Pembeli {get; set;}
 
         public Pesanan(string Id=null) : base(Id)
